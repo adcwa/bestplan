115c46a534a4b945d95b5fcb4cf685431df6e1f5
wangfeng7 <wangfeng7@lixiang.com>
2025-01-26 14:59:34 +0800

    build desktop

--- next.config.js
+++ next.config.js
@@ -16,13 +16,22 @@ const nextConfig = {
       config.optimization = {
         ...config.optimization,
         splitChunks: {
-          ...config.optimization.splitChunks,
+          chunks: 'all',
+          minSize: 20000,
+          minRemainingSize: 0,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          enforceSizeThreshold: 50000,
           cacheGroups: {
-            ...config.optimization.splitChunks.cacheGroups,
-            commons: {
-              name: 'commons',
-              chunks: 'all',
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
               minChunks: 2,
+              priority: -20,
               reuseExistingChunk: true,
             },
           },
